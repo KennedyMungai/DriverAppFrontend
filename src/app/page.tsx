5e270823d4baa19cffac380148b1e37cd0282e84
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-05 15:50:42 +0300

    Blank index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <main className="w-screen flex items-center justify-around bg-zinc-800"></main>
+    <main className="w-screen flex items-center justify-around bg-zinc-800 h-screen p-5"></main>
   );
 }
